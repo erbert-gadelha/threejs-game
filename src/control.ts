696a9ac77c514e096ad1ef857d148caef7720fcb
erbert-gadelha <ebgr@cin.ufpe.br>
2025-02-16 04:18:05 -0300

    [feat] suporte a gestos no touch

--- src/control.ts
+++ src/control.ts
@@ -7,6 +7,7 @@ export class Control {
     private camera:THREE.PerspectiveCamera;
     private raycaster:THREE.Raycaster  = new THREE.Raycaster();
     private mouse = new THREE.Vector2();
+    private input:{[key:string]:boolean} = {"mouse_0": false, "mouse_1": false, "mouse_2": false};
     private isMouseDown = false;
     private isDragging:boolean = false;
     private lastX = 0;
@@ -67,17 +68,26 @@ export class Control {
 
 
     public onMouseDown(event:MouseEvent) {
-        this.isMouseDown = true;
+        switch(event.button) {
+            case 0: this.input['mouse_0'] = true; break; // left
+            case 1: this.input['mouse_1'] = true; break; // middle
+            case 2: this.input['mouse_2'] = true; break; // right
+        }
         this.lastX = event.clientX;
         this.lastY = event.clientY;
         this.isDragging = false;
     }
-    public onMouseUp(/*event:MouseEvent*/) {
-        this.isMouseDown = false;
+    public onMouseUp(event:MouseEvent) {
+        switch(event.button) {
+            case 0: this.input['mouse_0'] = false; break; // left
+            case 1: this.input['mouse_1'] = false; break; // middle
+            case 2: this.input['mouse_2'] = false; break; // right
+        }
     }
 
     public onMouseMove(event:MouseEvent) {
-        if (this.isMouseDown) {
+
+        if (this.input['mouse_0']) {
             this.isDragging = true;
             const deltaX = this.lastX-event.clientX;
             const deltaY = this.lastY-event.clientY;
@@ -85,6 +95,17 @@ export class Control {
             this.board.rotation.x -= deltaY * 0.005;
             this.lastX = event.clientX;
             this.lastY = event.clientY;
+            this.render();
+        } else if(this.input['mouse_1']) {
+
+            const deltaX = this.lastX-event.clientX;
+            const deltaY = this.lastY-event.clientY;
+            this.lastX = event.clientX;
+            this.lastY = event.clientY;
+
+            this.camera.position.x+= deltaX/100;
+            this.camera.position.y-= deltaY/100;
+
             this.render();
         } else {
             const object:THREE.Object3D|null = this.rayCast(event);
@@ -120,6 +141,65 @@ export class Control {
 
     private clamp (min:number, value:number, max:number):number  {
         return Math.min(Math.max(value, min), max);
+    }
+
+
+    private touchDistance:number = 0;
+
+    public onTouchStart(event: TouchEvent) {
+        if(event.touches.length == 2) {
+            const x_t0 = event.touches[0].clientX;
+            const y_t0 = event.touches[0].clientY;
+            const x_t1 = event.touches[1].clientX;
+            const y_t1 = event.touches[1].clientY;
+            
+            const deltaX = x_t0 - x_t1;
+            const deltaY = y_t0 - y_t1;
+
+            this.touchDistance = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+            return;
+        }
+        const touch = event.touches[0];
+        this.lastX = touch.clientX;
+        this.lastY = touch.clientY;
+        event.preventDefault()
+    }
+    
+    public onTouchMove(event: TouchEvent) {
+
+        event.preventDefault()
+
+        if(event.touches.length == 1) {
+            const touch = event.touches[0];
+            const deltaX = (this.lastX - touch.clientX) * 0.005;
+            const deltaY = (this.lastY - touch.clientY) * 0.005;
+
+            console.log("", deltaX, deltaY);
+        
+            this.board.rotation.y -= deltaX;
+            this.board.rotation.x -= deltaY;
+        
+            this.lastX = touch.clientX;
+            this.lastY = touch.clientY;
+            
+        } else if (event.touches.length == 2) {
+            const x_t0 = event.touches[0].clientX;
+            const y_t0 = event.touches[0].clientY;
+            const x_t1 = event.touches[1].clientX;
+            const y_t1 = event.touches[1].clientY;
+            
+            const deltaX = x_t0 - x_t1;
+            const deltaY = y_t0 - y_t1;
+
+            const touchDistance_ = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+            //this.camera.position.z += (touchDistance_ - this.touchDistance)/10
+            const newPosition:number = this.camera.position.z - (touchDistance_ - this.touchDistance)/10;
+            this.camera.position.z = this.clamp(2, newPosition, 20);
+            this.touchDistance = touchDistance_;
+        }
+
+        this.render();
+        
     }
 
 
